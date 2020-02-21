a1ca4e83d04ed0e44dd8994a507873484d200491
Rachel Lew <rlew07@gmail.com>
2020-02-21 11:23:35 -0700

    Add reviews method

--- app/facades/book_search.rb
+++ app/facades/book_search.rb
@@ -10,4 +10,11 @@ class BookSearch
     book_data = service.book_by_title(title)
     book = Book.new(book_data)
   end
+
+  def reviews
+    service = NewYorkTimesService.new
+    service.reviews_by_title(title).map do |review_data|
+      Review.new(review_data)
+    end
+  end
 end
