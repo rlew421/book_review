0604fcc487387cd866bae7dceb88ea3c9bdfb2fc
Rachel Lew <rlew07@gmail.com>
2020-02-21 10:45:06 -0700

    Move API connection and getting/parsing of JSON into private methods

--- app/services/open_library_service.rb
+++ app/services/open_library_service.rb
@@ -1,11 +1,18 @@
 class OpenLibraryService
   def book_by_title(title)
+    get_json("?title=#{title}")
+  end
+
+  private
+
+  def get_json(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)[:docs].first
+  end
+
+  def conn
     conn = Faraday.new(url: "http://openlibrary.org/search.json") do |faraday|
       faraday.adapter Faraday.default_adapter
     end
-
-    response = conn.get("?title=#{title}")
-
-    parsed = JSON.parse(response.body, symbolize_names: true)[:docs].first
   end
 end
